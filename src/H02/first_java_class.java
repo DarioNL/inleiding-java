494643aace197e3ff837345030fee458c1f33912
HP 8470p <Darioponzo03@gmail.com>
2019-09-03 10:48:04 +0200

    edit

--- src/H02/first_java_class.java
+++ src/H02/first_java_class.java
@@ -1,4 +1,16 @@
 package H02;
 
+import java.awt.*;
+import java.applet.*;
+
 public class first_java_class {
+
+public void init (){
+
+}
+
+public void paint(Graphics g) {
+    g.drawString("welcome to java!!", 50, 60 );
+ }
+
 }
