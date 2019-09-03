9a9b428b5c23c101d1ecc89aca123e31ec35067e
HP 8470p <Darioponzo03@gmail.com>
2019-09-03 14:33:32 +0200

    edit

---
+++ src/h04/gewicht.java
@@ -0,0 +1,27 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class gewicht extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.CYAN);
+        g.setColor(Color.BLACK);
+        g.fillRect(10, 10, 50, 100);
+        g.setColor(Color.WHITE);
+        g.fillRect(70, 30, 50, 80);
+        g.setColor(Color.RED);
+        g.fillRect(130, 70, 50, 40);
+
+
+    }
+
+
+
+
+
+}
