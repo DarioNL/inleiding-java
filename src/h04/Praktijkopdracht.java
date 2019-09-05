f43bcdddeb50a8b58d8153d198c00ad0ad87e500
HP 8470p <Darioponzo03@gmail.com>
2019-09-05 09:35:05 +0200

    edit

--- src/h04/Praktijkopdracht.java
+++ src/h04/Praktijkopdracht.java
@@ -12,17 +12,17 @@ public class Praktijkopdracht extends Applet {
         setBackground(Color.WHITE);
         g.setColor(Color.BLACK);
         g.setColor(Color.magenta);
-        g.fillRect(290,80, 230, 50);
-        g.fillOval(290,150,230,70);
-        g.fillArc(530,80,230,50,0,45);
+        g.fillRect(170,80, 100, 50);
+        g.fillOval(170,150,100,70);
+        g.fillArc(290,80,100,50,0,45);
 
         g.setColor(Color.BLACK);
-        g.drawLine(50, 20, 270, 20);
-        g.drawRoundRect(50, 150, 230, 70, 30, 30);
-        g.drawRect(50, 80, 230, 50);
-        g.drawArc(290,80,230,50, 0,360);
-        g.drawArc(530, 80, 230, 50, 0, 360);
-        g.drawRoundRect(600, 150, 75, 75, 100, 200);
+        g.drawLine(50, 20, 150, 20);
+        g.drawRoundRect(50, 150, 100, 70, 30, 30);
+        g.drawRect(50, 80, 100, 50);
+        g.drawArc(170,80,100,50, 0,360);
+        g.drawArc(290, 80, 100, 50, 0, 360);
+        g.drawRoundRect(300, 150, 75, 75, 100, 200);
 
 
     }
