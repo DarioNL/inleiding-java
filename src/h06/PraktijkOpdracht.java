47bd639563ecb57826cff5ca6d1cb5438958b972
HP 8470p <Darioponzo03@gmail.com>
2019-09-06 10:37:43 +0200

    edit

--- src/h06/PraktijkOpdracht.java
+++ src/h06/PraktijkOpdracht.java
@@ -5,16 +5,27 @@ import java.awt.*;
 
 public class PraktijkOpdracht extends Applet {
 
-    double gemiddelde1, gemiddelde2, gemiddelde3, deel, andtwoord;
+    double gemiddelde1, gemiddelde2, gemiddelde3, deel, andtwoord1, antwoord2, antwoord3, antwoord4;
+    int antwoordint;
 
     @Override
     public void init() {
         gemiddelde1= 5.9;
         gemiddelde2= 6.3;
         gemiddelde3= 6.9;
+
         deel=3;
-        andtwoord = (gemiddelde1 + gemiddelde2 + gemiddelde3)/deel;
-        
+
+        andtwoord1 = (gemiddelde1 + gemiddelde2 + gemiddelde3)/deel;
+        antwoord2 = (andtwoord1 * 10);
+
+        antwoordint = (int) antwoord2;
+        antwoord3 = antwoordint;
+
+        antwoord4= antwoord3 /10;
+
+
+
     }
 
 
@@ -22,7 +33,7 @@ public class PraktijkOpdracht extends Applet {
 
     @Override
     public void paint(Graphics g) {
-        g.drawString("het gemiddelde is " + andtwoord , 20, 20);
+        g.drawString("het gemiddelde is " + antwoord4 , 20, 20);
 
 
     }
