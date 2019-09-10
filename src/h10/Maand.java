628138a5a84d4bf6b07caaa107d676fdc69f97d2
HP 8470p <Darioponzo03@gmail.com>
2019-09-10 13:07:19 +0200

    edit

--- src/h10/Maand.java
+++ src/h10/Maand.java
@@ -8,13 +8,20 @@ import java.awt.event.ActionListener;
 
 public class Maand extends Applet {
     TextField tekstvak;
+    TextField tekstvak2;
     Label label;
-    String s, tekst;
-    int maand;
+    Label label2;
+    String s, tekst, tekst2, s2;
+    int maand,jaartal;
 
     public void init() {
         tekstvak = new TextField("", 20);
         label = new Label("Typ het nummer van de maand en druk op enter");
+        tekstvak2 = new TextField("jaar", 20);
+        label2 = new Label("Typ het nummer van het jaar en druk op enter");
+        tekstvak2.addActionListener(new TekstvakListener() );
+        tekst2 = "";
+        add(tekstvak2);
         tekstvak.addActionListener( new TekstvakListener() );
         tekst = "";
         add(label);
@@ -28,49 +35,98 @@ public class Maand extends Applet {
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             s = tekstvak.getText();
-            maand = Integer.parseInt( s);
-            switch(maand) {
-                case 1:
-                    tekst = "januari heeft 31 dagen";
-                    break;
-                case 2:
-                    tekst = "feberuari heeft 28 dagen";
-                    break;
-                case 3:
-                    tekst = "maart heeft 31 dagen";
-                    break;
-                case 4:
-                    tekst = "april heeft 30 dagen";
-                    break;
-                case 5:
-                    tekst = "mei heeft 31 dagen";
-                    break;
-                case 6:
-                    tekst = "juni heeft 30 dagen";
-                    break;
-                case 7:
-                    tekst = "juli heeft 31 dagen";
-                    break;
-                case 8:
-                    tekst = "augustus heeft 31 dagen";
-                    break;
-                case 9:
-                    tekst = "sebtember heeft 30 dagen";
-                    break;
-                case 10:
-                    tekst = "oktober heeft 31 dagen";
-                    break;
-                case 11:
-                    tekst = "november heeft 30 dagen";
-                    break;
-                case 12:
-                    tekst = "december heeft 31 dagen";
-                    break;
-                default:
-                    tekst = "U hebt een verkeerd nummer ingetikt ..!";
-                    break;
+            s2 = tekstvak2.getText();
+            jaartal = Integer.parseInt( s2);
+            if ( (jaartal % 4 == 0 && !(jaartal % 100 == 0)) ||
+                    jaartal % 400 == 0 ) {
+                maand = Integer.parseInt( s);
+                switch(maand) {
+                    case 1:
+                        tekst = "januari heeft 31 dagen";
+                        break;
+                    case 2:
+                        tekst = "feberuari heeft 29 dagen";
+                        break;
+                    case 3:
+                        tekst = "maart heeft 31 dagen";
+                        break;
+                    case 4:
+                        tekst = "april heeft 30 dagen";
+                        break;
+                    case 5:
+                        tekst = "mei heeft 31 dagen";
+                        break;
+                    case 6:
+                        tekst = "juni heeft 30 dagen";
+                        break;
+                    case 7:
+                        tekst = "juli heeft 31 dagen";
+                        break;
+                    case 8:
+                        tekst = "augustus heeft 31 dagen";
+                        break;
+                    case 9:
+                        tekst = "sebtember heeft 30 dagen";
+                        break;
+                    case 10:
+                        tekst = "oktober heeft 31 dagen";
+                        break;
+                    case 11:
+                        tekst = "november heeft 30 dagen";
+                        break;
+                    case 12:
+                        tekst = "december heeft 31 dagen";
+                        break;
+                    default:
+                        tekst = "U hebt een verkeerd nummer ingetikt ..!";
+                        break;
+                }
+                repaint();
             }
-            repaint();
+            else {
+                maand = Integer.parseInt( s);
+                switch(maand) {
+                    case 1:
+                        tekst = "januari heeft 31 dagen";
+                        break;
+                    case 2:
+                        tekst = "feberuari heeft 28 dagen";
+                        break;
+                    case 3:
+                        tekst = "maart heeft 31 dagen";
+                        break;
+                    case 4:
+                        tekst = "april heeft 30 dagen";
+                        break;
+                    case 5:
+                        tekst = "mei heeft 31 dagen";
+                        break;
+                    case 6:
+                        tekst = "juni heeft 30 dagen";
+                        break;
+                    case 7:
+                        tekst = "juli heeft 31 dagen";
+                        break;
+                    case 8:
+                        tekst = "augustus heeft 31 dagen";
+                        break;
+                    case 9:
+                        tekst = "sebtember heeft 30 dagen";
+                        break;
+                    case 10:
+                        tekst = "oktober heeft 31 dagen";
+                        break;
+                    case 11:
+                        tekst = "november heeft 30 dagen";
+                        break;
+                    case 12:
+                        tekst = "december heeft 31 dagen";
+                        break;
+                    default:
+                        tekst = "U hebt een verkeerd nummer ingetikt ..!";
+                        break;
+                }
+                repaint(); }
         }
     }
 }
